8abcf13eded5781f5bbb60b254c3d1b4321cdbc1
Ahmed <0423rafi@gmail.com>
2023-11-26 12:57:42 +0400

    formatted code

--- adduser.php
+++ adduser.php
@@ -1,7 +1,7 @@
 <?php
 require('connection.php');
 
-// Check if the form is submitted
+
 if (isset($_POST['submit'])) {
     // Get form data
     $user_fname = $_POST['full-name'];
@@ -11,14 +11,12 @@ if (isset($_POST['submit'])) {
     $user_password = $_POST['password'];
     $user_confirmpassword = $_POST['confirm-password'];
 
-    // Specify the columns in the INSERT statement
+
     $sql = "INSERT INTO users (user_fname, user_email, phone_number, user_role, user_password, user_confirmpassword) 
             VALUES ('$user_fname', '$user_email', '$phone_number', '$user_role', '$user_password', '$user_confirmpassword')";
 
-    // Execute the query
     $result = mysqli_query($conn, $sql);
 
-    // Check for errors in query execution
     if ($result) {
         echo "<script>alert('New Users Registered Successfully')</script>";
         echo "<script>window.open('signup.php','_self')</script>";
