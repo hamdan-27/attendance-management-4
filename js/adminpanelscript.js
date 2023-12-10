290f6e2247be8acb5eda80f9d78ac47429b5196a
hamdan-27 <hamdan102703@gmail.com>
2023-12-10 14:38:09 +0400

    changed loadContent function

--- js/adminpanelscript.js
+++ js/adminpanelscript.js
@@ -1,6 +1,6 @@
 function loadContent(page) {
 
-    fetch(page + '.html') 
+    fetch(page) 
       .then(response => response.text())
       .then(data => {
         document.getElementById('content').innerHTML = data;
