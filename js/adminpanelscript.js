5f7339bcbec34a98edf6d393638d49cec20695fe
hamdan-27 <hamdan102703@gmail.com>
2023-12-15 02:12:28 +0400

    commented out unnecessary code

--- js/adminpanelscript.js
+++ js/adminpanelscript.js
@@ -6,10 +6,10 @@ function loadContent(page) {
         document.getElementById('content').innerHTML = data;
         // Manually execute scripts if needed
         // e.g., if there are script tags in the fetched content
-        const scripts = document.getElementById('container').getElementsByTagName('script');
-        for (let script of scripts) {
-          eval(script.innerHTML);
-    }
+    //     const scripts = document.getElementById('container').getElementsByTagName('script');
+    //     for (let script of scripts) {
+    //       eval(script.innerHTML);
+    // }
     // Reattach event listeners if needed
     // e.g., if there are event listeners in the fetched content
     // document.getElementById('yourButton').addEventListener('click', yourFunction);
